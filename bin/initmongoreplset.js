e896a5f48d58fb1fbd745e5659bb830b16029e6a
SingLi <Sing-Li@users.noreply.github.com>
2017-01-31 23:24:10 -0500

    Fix occasional startup hold
    `rs.initiate()` causes default to sometimes, under some runtime environments, to select the wrong hostname.

--- bin/initmongoreplset.js
+++ bin/initmongoreplset.js
@@ -3,6 +3,6 @@ printjson(ism);
 if (ism.ismaster) {
    } else 
 {
-    var msg  = rs.initiate();
+    var msg  = rs.initiate( { _id: "rcreplset", members:[{_id: 0, host: "127.0.0.1:27017"}]});
     printjson(msg);
 }
