61aba4c976cdd05cf8ea43eec29e578e25e98f76
MZN Lab <elespike@mznlab.net>
2017-02-11 03:30:54 -0500

    Preliminary multi-arch Added post-stop script for service restart delay

---
+++ stagemongo.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# TODO Instead of the architecture, this needs to verify the currently installed mongo version, if any
+if [[ $(uname -m) == "x86_64" ]]
+then
+    wget "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.2.7.tgz"
+    tar -zxf ./mongodb-linux-x86_64-ubuntu1604-3.2.7.tgz --strip-components=1
+else
+    IFS=" " read -a links <<< $(apt-get -y --print-uris install mongodb | egrep -o "https?://[^']+")
+    for link in ${links[@]}
+    do
+        wget ${link}
+    done
+
+    IFS=" " read -a deb_pkgs <<< $(ls ./ | egrep -o "mongo.+\.deb")
+    for pkg in ${deb_pkgs[@]}
+    do
+        dpkg-deb -R ${pkg} ./
+    done
+fi
+
