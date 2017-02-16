1308b277a5af21849c9db6f2f33317aaa5546366
MZN Lab <elespike@mznlab.net>
2017-02-15 23:01:48 -0500

    Update stagemongo.sh

--- stagemongo.sh
+++ stagemongo.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-# TODO Instead of the architecture, this needs to verify the currently installed mongo version, if any
+# TODO move this to /bin if possible
+
+# TODO try this out
+# [[ -n $(which wget) ]] || apt-get -y wget
+
 if [[ $(uname -m) == "x86_64" ]]
 then
     wget "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.2.7.tgz"
