a9f0d13ba0c030df7433ee9c95eca3bc880ca3d6
MZN Lab <elespike@mznlab.net>
2017-02-17 01:29:38 -0500

    Fixed some naming kinks

--- bin/preparemongo.sh
+++ bin/preparemongo.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
-cd ../
-
 if [[ $(uname -m) == "x86_64" ]]
 then
-    wget "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.2.7.tgz"
+    wget --backups=0 "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.2.7.tgz"
     tar -zxf ./mongodb-linux-x86_64-ubuntu1604-3.2.7.tgz --strip-components=1
 else
     IFS=" " read -a links <<< $(apt-get -y --print-uris install mongodb | egrep -o "https?://[^']+")
     for link in ${links[@]}
     do
-        wget ${link}
+        wget --backups=0 ${link}
     done
 
     IFS=" " read -a deb_pkgs <<< $(ls ./ | egrep -o "mongo.+\.deb")
