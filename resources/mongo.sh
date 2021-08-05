b6a79eaa058578233d3166aa3cf5486978f628b0
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2021-08-05 20:56:44 +0530

    remove try_start loop
    If started as a daemon, the parent mongodb process waits for the child
    to be ready to accept connections before exiting. The rocketchat-mongo
    app now takes care of the logs. The "after" keys make sure both
    rocketchat-{server,mongo} starts after mongodb is ready for connections
    and the log file is there to be read. Since the service type (or daemon
    type) is "forking", units that depend on this, are not going to start
    unless the parent process exits.

---
+++ resources/mongo.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+start() {
+	LC_ALL=C mongod --bind_ip 127.0.0.1 --port 27017 \
+		--pidfilepath $SNAP_COMMON/mongod.pid \
+		--logpath=$SNAP_COMMON/mongod.log \
+		--dbpath=$SNAP_COMMON \
+		--smallfiles --journal --replSet rs0 \
+		--fork
+}
+
+stop() {
+	LC_ALL=C mongod --dbpath=$SNAP_COMMON --shutdown
+}
+
+logs() {
+	tail -f $SNAP_COMMON/mongod.log
+}
+
+shell() {
+	LC_ALL=C mongo
+}
+
+if [ -z "$1" ]; then
+	shell
+else
+	eval "${1#--}"
+fi
