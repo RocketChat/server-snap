428a7a90be263ce659a85e33c870aa48b96a4291
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2021-11-22 19:29:36 +0530

    Make migration not overwrite existing config files
    Signed-off-by: Debdut Chakraborty <debdut.chakraborty@rocket.chat>

--- migrations/post_new_snap_initializations.sh
+++ migrations/post_new_snap_initializations.sh
@@ -1,4 +1,7 @@
-cat >$SNAP_DATA/mongod.conf <<EOF
+#!/bin/bash
+
+init_mongod_conf() {
+  cat >$SNAP_DATA/mongod.conf <<EOF
 
 # For documentation of all options, see:
 #   http://docs.mongodb.org/manual/reference/configuration-options/
@@ -27,19 +30,26 @@ processManagement:
   pidFilePath: $SNAP_COMMON/mongod.pid
 
 EOF
+}
 
-port=$(snapctl get port)
-[[ -z $port ]] && snapctl set port=3000
-url=$(snapctl get caddy-url)
-[[ -z $url ]] && url=$(snapctl get siteurl)
-[[ -n $url ]] && snapctl set siteurl=$url || snapctl set siteurl=http://localhost:$port
+init_default_snap_configurations() {
+  port=$(snapctl get port)
+  [[ -z $port ]] && snapctl set port=3000
 
-[[ -z $(snapctl get mongo-url) ]] && snapctl set mongo-url=mongodb://localhost:27017/parties
-[[ -z $(snapctl get mongo-oplog-url) ]] && snapctl set mongo-oplog-url=mongodb://localhost:27017/local
-[[ -z $(snapctl get backup-on-refresh) ]] && snapctl set backup-on-refresh=disable
+  url=$(snapctl get caddy-url)
+  [[ -z $url ]] && url=$(snapctl get siteurl)
+  [[ -n $url ]] && snapctl set siteurl=$url || snapctl set siteurl=http://localhost:$port
 
-snapctl unset snap-refreshing
-snapctl unset caddy
-snapctl unset caddy-url
-snapctl unset https
-snapctl unset db-feature-compatibility-version
+  [[ -z $(snapctl get mongo-url) ]] && snapctl set mongo-url=mongodb://localhost:27017/parties
+  [[ -z $(snapctl get mongo-oplog-url) ]] && snapctl set mongo-oplog-url=mongodb://localhost:27017/local
+  [[ -z $(snapctl get backup-on-refresh) ]] && snapctl set backup-on-refresh=disable
+
+  snapctl unset snap-refreshing
+  snapctl unset caddy
+  snapctl unset caddy-url
+  snapctl unset https
+  snapctl unset db-feature-compatibility-version
+}
+
+[[ -f $SNAP_DATA/mongod.conf ]] || init_mongod_conf
+init_default_snap_configurations
