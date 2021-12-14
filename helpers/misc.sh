80cfc58ca9d3df236127d1c121abd3e38a836566
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2021-12-15 00:56:01 +0530

    prettier config and miscellaneous function lib
    Signed-off-by: Debdut Chakraborty <debdut.chakraborty@rocket.chat>

---
+++ helpers/misc.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+init_user_environment_variables() {
+  # Check both $SNAP_COMMON and $SNAP_DATA
+  # I was hoping this to work without a for loop like this
+  # find $SNAP_COMMON $SNAP_DATA -maxdepth 1 -regex '.*\.env$' \
+  #  | while read filename; do source $filename; done
+  set -a
+  for filename in $(find $SNAP_COMMON $SNAP_DATA -maxdepth 1 -regex '.*\.env$'); do source $filename; done
+  set +a
+}
